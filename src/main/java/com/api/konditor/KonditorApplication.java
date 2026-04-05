3b84d6813066744466c7080eb84b4161d8f7d076
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2026-04-04 22:59:30 -0300

    first

---
+++ src/main/java/com/api/konditor/KonditorApplication.java
@@ -0,0 +1,13 @@
+package com.api.konditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/konditor/KonditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.konditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
